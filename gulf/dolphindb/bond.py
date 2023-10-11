2519a231720a4fca9619fa9cbbcbe0758d4970b7
fangyang_gpu <fangyang.jing@hotmail.com>
2023-10-11 22:17:36 +0800

    [Add] bond add query func

--- gulf/dolphindb/bond.py
+++ gulf/dolphindb/bond.py
@@ -5,7 +5,7 @@
 # @Software : PyCharm
 
 import datetime
-from typing import Union, Dict
+from typing import Union, Dict, List
 
 import numpy as np
 import pandas as pd
@@ -59,7 +59,7 @@ class BondDB(Dolphindb):
         db=database('{DfsDbPath.bond_daily_code}', COMPO, [db_code_hash,db_date_range], engine=`{self.engine.value})
         """
 
-    def download_bond_daily_table(self):
+    def update_bond_daily_table_by_akshare(self):
         """
         优点: 数据全面, 包含退市债可以达到 48w 数据量, 有纯债价值和纯债溢价率, 但是意义不大在 schema 中注释掉
         缺点:
@@ -138,17 +138,115 @@ class BondDB(Dolphindb):
         bond_basic_df = self.get_dimension_table_df(BondBasicTable, from_db=False)
         bond_redeem_df = self.get_dimension_table_df(BondRedeemTable, from_db=False)
 
+    def get_bond_daily_table_df(
+            self, table_name: str = "temp_table",
+            start_date: str = "2020.01.01",
+            end_date: str = "",
+            jj_code_list: List[str] = None,
+            fields: List[str] = None,
+            ctx_by_date_filters: List[str] = None,
+            is_indclass_onehot: bool = False,
+            end_sql: str = ""
+    ) -> pd.DataFrame:
+        """
+        日常使用习惯方便的sql封装, 增加一些参数可以指定过滤
+        1. 连表了StockBasicTable, 增加了行业, 股本, 每股净资产数据
+        2. 行业信息可以通过参数指定是否需要onehot
+        3. 可扩展的 end_sql
+
+        :param table_name:
+        :param start_date, "2005.01.01"
+        :param end_date, 格式同上, 默认值为 "", 表示到数据库中最新的数据
+        :param jj_code_list: ['SHSE.113635']
+        :param fields: List[str], ['bond_scale', "stock_code", "trans_stock_premium"], 全部字段参考bond_daily_table.schema
+        :param ctx_by_date_filters:
+        :param is_indclass_onehot: 是否行业 onehot encode
+        :param end_sql:
+        :return: 返回 bond daily Dataframe
+        """
+        from gulf.dolphindb.tables.dimension.stock_tables import StockBasicTable
+
+        dt = ""
+        ctx_by_code_filters = []
+
+        if start_date:
+            dt += f"trade_date>={start_date} "
+        if end_date:
+            dt += f"&& trade_date<={end_date} "
+
+        if dt:
+            ctx_by_code_filters.append(dt)
+
+        if jj_code_list:
+            ctx_by_code_filters.append(f"jj_code in {jj_code_list}")
+        # jj_code_filter = f"jj_code in {jj_code_list}" if jj_code_list else ""
+
+        fields_str = "," + ",".join(fields) if fields else ""
+
+        # filters = ", ".join([dt, jj_code_filter])
+        ctx_by_code_filters_str = ", ".join(ctx_by_code_filters)
+
+        if ctx_by_date_filters:
+            filter_ctx_by_date = f"""
+                    bond_table = select * from bond_table 
+                    where {', '.join(ctx_by_date_filters)} 
+                    context by trade_date;
+                """
+        else:
+            filter_ctx_by_date = ""
+
+        res = f"""
+                use ta;
+                db_path = "{bond_daily_table.db_path}";
+                db = database(db_path);
+                t = loadTable(db, "{bond_daily_table.name}");
+
+                bond_table = select jj_code, bond_name, stock_name, trade_date, stock_code as securityid,
+                open, high, low, close, volume as vol, rowAvg(close, high, low, open) * volume as amount, 
+                (ratios(close)-1)*100 as pct_chg, cumwavg(rowAvg(close, high, low), volume) as vwap,  
+                bond_scale, trans_stock_premium, duallow {fields_str}  
+                from t 
+                where {ctx_by_code_filters_str} 
+                context by jj_code;
+
+                {filter_ctx_by_date}
+
+                table6_name = "{StockBasicTable.name}";
+                db6_path = "{StockBasicTable.db_path}";
+                db6 = database(db6_path);
+                table6 = loadTable(db6, table6_name);
+                // alpha101 infoData , 没有 cap 总市值, 只有行业信息, 因为cap只与 alpha56 有关
+
+                infoData = select 
+                代码 as securityid, 所处行业 as indclass, 
+                已流通股份 as stock_cshare, 总股本 as stock_tshare, 每股净资产 as stock_nav  
+                from table6 where 所处行业!=NULL;
+
+                {f'infoData = oneHot(infoData, `indclass);' if is_indclass_onehot else ''}
+
+                {table_name} = select * from ej(bond_table, infoData, `securityid) order by jj_code, trade_date;
+
+                {end_sql}
+
+                {table_name};
+                """
+
+        return self.session.run(res)
+
 
 if __name__ == '__main__':
     db = BondDB()
 
-    db.update_dimension_tables()
+    # db.update_dimension_tables()
     # bond_basic_df = db.get_dimension_table_df(BondBasicTable, from_db=True)
 
     # Note: BondBasicTable 从网上获取的全部转债, 
     # 如果包含退市债, 历史日数据 47 万
     # 如果不包含退市债, 历史日数据 24 万
     # sina 网络的接口有限速, 当前配置可能需要调整
-    # db.download_bond_daily_table()
+    # db.update_bond_daily_table_by_akshare()
 
-    db.update_bond_daily_table_by_reader(offset=-1)
+    # db.update_bond_daily_table_by_reader(offset=-1)
+
+    df = db.get_bond_daily_table_df(start_date='2023.10.10', is_indclass_onehot=True)
+    print(1)
